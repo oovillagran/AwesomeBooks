0a19a923292e761c4a947eb5bf51e3140a1c49bb
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-27 15:49:21 -0600

    Added the book's list dynamically on the librery section

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // array to keep the books
 
-let stock = [];
+let stock = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+];
 
 // Add a new object into the array when user sends the form
 
@@ -10,3 +15,28 @@ function addBook() {
     const newBook = {title: title, author: author};
     stock.push(newBook);
   }
+
+
+
+// To show the book in the stock \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// esto es para la informacion del libro
+let book = '';
+
+//here i put the books one by one
+stock.forEach((element) => {
+  book += `
+    <div>
+    <h4>${element.title}</h4>
+    <p>${element.author}</p>
+    </div>
+    <button class="remove" type="button" onclick="removeBook">Remove</button>
+    <hr>
+  `;
+});
+
+//call the father element
+const section = document.getElementById('library');
+
+section.innerHTML = book;
+

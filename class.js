ec3358767fbc3d24030579b1f5b4220affd77b08
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-30 12:15:01 -0600

    Added the logo, the general styles and SPAs functionality

--- class.js
+++ class.js
@@ -105,3 +105,43 @@ addButton.addEventListener('click', (event) => {
 const date = new Date();
 const timeDate = document.querySelector('#date');
 timeDate.innerHTML = date.toString();
+
+// For the single page functionality
+
+// Get nav bar links
+const linkList = document.querySelector('#link-list');
+const linkAdd = document.querySelector('#link-add');
+const linkContact = document.querySelector('#link-contact');
+
+// Get page sections ID
+const bookListSection = document.querySelector('#book-list');
+const addSection = document.querySelector('#add-section');
+const contactSection = document.querySelector('#contact-info');
+
+// Listeners for the links events
+linkList.addEventListener('click', function(event) {
+  event.preventDefault();
+  bookListSection.style.display = "flex";
+  addSection.style.display = "none";
+  contactSection.style.display = "none";
+});
+
+linkAdd.addEventListener('click', function(event) {
+  event.preventDefault();
+  bookListSection.style.display = "none";
+  addSection.style.display = "flex";
+  contactSection.style.display = "none";
+});
+
+linkContact.addEventListener('click', function(event) {
+  event.preventDefault();
+  bookListSection.style.display = "none";
+  addSection.style.display = "none";
+  contactSection.style.display = "flex";
+});
+
+
+
+
+
+
